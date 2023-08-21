9fb8aae4bb2830e4c545d7602c9ba88508cab33b
ashap5 <pashaust2@gmail.com>
2023-08-21 14:41:38 +0200

    fixed

--- reddimgfix.js
+++ reddimgfix.js
@@ -1,24 +1,15 @@
-// Check if the URL contains "https://preview.redd.it"
 if (location.href.includes("https://preview.redd.it")) {
-  // Extract the image URL from the query parameter
-  const match = location.search.match(/[?&]url=([^&]+)/);
-  if (match) {
-    const imageUrl = decodeURIComponent(match[1]);
+  const imageUrl = convertRedditUrl(location.search);
 
-    // Modify the URL as needed
-    const newUrl = convertRedditUrl(imageUrl);
-
-    if (newUrl) {
-      // Redirect to the new URL
-      location.replace(newUrl);
-    } else {
-      // Handle cases where the URL can't be modified
-      alert("Invalid URL or pattern not matched.");
-    }
+  if (imageUrl) {
+    location.replace(imageUrl);
+  } else {
+    alert("Invalid URL or pattern not matched.");
   }
 }
 
-function convertRedditUrl(inputUrl) {
-  const match = inputUrl.match(/\/([a-zA-Z0-9]+\.jpg)\?.*$/);
-  return match ? `https://www.reddit.com/media?url=https://i.redd.it/${match[1]}` : null;
+function convertRedditUrl(queryString) {
+    const imageUrl = decodeURIComponent(queryString);
+    const imageMatch = imageUrl.match(/\/([a-zA-Z0-9]+\.jpg)\?.*$/);
+    return imageMatch ? `https://www.reddit.com/media?url=https://i.redd.it/${imageMatch[1]}` : null;
 }
